c9fa4cc4bba3f21d56c1c0f75f26eb9de6320af6
Antcharles22 <121199104+Antcharles22@users.noreply.github.com>
2024-09-06 10:25:15 +0100

    first commit

---
+++ portfollio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Nav } from './me/nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      
+    </div>
+  );
+}
+
+export default App;
